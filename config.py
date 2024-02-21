8cf44372eec7cb52027a2feeb368caabf3c0eecb
Ian Stride <ianstride@gmail.com>
2024-02-21 19:03:28 +0000

    Use 'sg_sources' instead of 'sg_path' in config

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
+# For details of what the various configuration settings are for see the
+# configuration documentation at:
+# https://github.com/IDEMSInternational/parenttext-pipeline/blob/main/docs/configuration.md
+
 # Data sources, IDs of Google Sheets where the core date is stored.
 # Specific for ZA.
 localised_sheets = "13do_Qnc0VKC6Ao4N7YY3skUFKJMuwOixj2GyMVwnRLM"
@@ -16,24 +20,6 @@ C_goal_checkin_ID = "1gympuD5KdlAdDJSuaVQiXjWSwJxoDcA9K-oBRyKmS7o"
 C_dev_asess_tool_ID = "1OhhQF5yarUDmaSl2tlt7eIT7wJ8bGwNFzI3BOplJYsc"
 safeguarding = "1PHgUhJnZdE0lK6C9teK-hwA6Tf-6Pgj1_OVdxoTgVOA"
 
-# "filename" is how it will be generally named in the pipeline.
-#
-# "crowdin_name" will be the name of the file that is produced to send to the
-# translators.
-#
-# "tags" are used to identify flows to be process. Possible values for tag 1:
-#   onboarding
-#   dev_assess
-#   ltp_activity
-#   home_activity_checkin
-#   module
-#   goal_checkin
-#   safeguarding
-#   menu
-#   delivery
-#
-# "split_no" is used to divide the file at the final step to get it to a manageable
-# size that can be uploaded to RapidPro.
 sources = [
     {
         "filename": "parenttext_all",
@@ -60,77 +46,6 @@ sources = [
     },
 ]
 
-# Data used when modifying expiration times.
-special_expiration = "./edits/specific_expiration.json"
-default_expiration = 1440
-
-# Model that is used as part of the process when the data is extracted from sheets.
-model = "models.parenttext_models"
-
-# Languages that will be looked for to localize back into the flows, "language" is the
-# 3-letter code used in RapidPro, "code" is the 2 letter code used in CrowdIn.
-languages = [
-    {"language": "hau", "code": "ss"},
-    {"language": "zul", "code": "zu"}
-]
-
-# Location where translations are stored, at the moment pointing to a locally cloned
-# repo, should maybe be adapted so we can provide a link to an online repo.
-translation_repo = "https://github.com/IDEMSInternational/plh-digital-content"
-folder_within_repo = "translations/parent_text_v2"
-
-# In one of the latter stages we have the option to modify the quick replies:
-# 1 - We may want to remove the quick replies and add them to message text and give
-#     numerical prompts to allow basic phone users to use the app - for this use
-#     reference code "move"
-# 2 - We may want to reformat the quick replies so that long ones are added to the
-#     message text as above - for this use reference code "reformat"
-# 3 - We may not want to do anything, for this use reference code "none"
-qr_treatment = "reformat"
-
-# This is the default phrase we want to add in if the quick replies are being moved to
-# message text.
-select_phrases = "./edits/select_phrases.json"
-
-# If we are in scenario 1 above, we may wish to add some basic numerical quick
-# replies back in, if so we need to specify add_selectors as True
-add_selectors = "yes"
-
-# Words we always want to keep as full quick replies are specified in this file.
-special_words = "./edits/special_words.json"
-
-# In scenario 2 we set limits on the number of quick replies and the length of the
-# quick replies.
-#   count_threshold (relates to number of quick replies)
-#   length_threshold (relates to length of the longest quick reply)
-# If the number of QRs is below or equal the count_threshold and the longest QR is
-# shorter than or equal to the length_threshold then the QR are to be left in place
-# the node will not be changed.
-# In places where the QR are too long. We will make the changes to make the QRs
-# numbers and add the number references to the message text as example 1.
-count_threshold = "3"
-length_threshold = "18"
-
-# Google Sheet ID containing AB testing data.
-# Same for all deployments.
-ab_testing_sheet_ID = "1i_oqiJYkeoMsYdeFOcKlvvjnNCEdQnZlsm17fgNvK0s"
-# South Africa specific.
-localisation_sheet_ID = "1FfO-LLjodgEKaBVnn47QrvXaM68Cvui55FS1DKziA2c"
-
-# Google Sheet ID containing dict edits data.
-# Same for all deployments.
-eng_edits_sheet_ID = "1Ab8H_s26EuOiS4nZ6HGADjD4CZw55586LL66fl8tEWI"
-# South Africa specific.
-transl_edits_sheet_ID = "1fCLPfiqHy1nLLqh1qyvd3zrziw5Tz3uQ6_e7CyuEW-E"
-
-# Data used in safeguarding script.
-SG_flow_ID = "b83315a6-b25c-413a-9aa0-953bf60f223c"
-SG_flow_name = "safeguarding_wfr_interaction"
-
-# Path to file containing translated safeguarding words.
-SG_path = "./edits/safeguarding_words.json"
-
-# Names of redirect flows to be modified as part of safeguarding process.
 redirect_flow_names = (
     '['
     '    "safeguarding_redirect_to_topic_all", '
@@ -142,25 +57,31 @@ redirect_flow_names = (
 
 def create_config():
     return {
-        "ab_testing_sheet_id": ab_testing_sheet_ID,
-        "add_selectors": add_selectors,
-        "count_threshold": count_threshold,
-        "default_expiration": default_expiration,
-        "eng_edits_sheet_id": eng_edits_sheet_ID,
-        "folder_within_repo": folder_within_repo,
-        "languages": languages,
-        "length_threshold": length_threshold,
-        "localisation_sheet_id": localisation_sheet_ID,
-        "model": model,
-        "qr_treatment": qr_treatment,
+        "ab_testing_sheet_id": "1i_oqiJYkeoMsYdeFOcKlvvjnNCEdQnZlsm17fgNvK0s",
+        "add_selectors": "yes",
+        "count_threshold": "3",
+        "default_expiration": 1440,
+        "eng_edits_sheet_id": "1Ab8H_s26EuOiS4nZ6HGADjD4CZw55586LL66fl8tEWI",
+        "folder_within_repo": "translations/parent_text_v2",
+        "languages": [
+            {"language": "hau", "code": "ss"},
+            {"language": "zul", "code": "zu"}
+        ],
+        "length_threshold": "18",
+        "localisation_sheet_id": "1FfO-LLjodgEKaBVnn47QrvXaM68Cvui55FS1DKziA2c",
+        "model": "models.parenttext_models",
+        "qr_treatment": "reformat",
         "redirect_flow_names": redirect_flow_names,
-        "select_phrases": select_phrases,
-        "sg_flow_id": SG_flow_ID,
-        "sg_flow_name": SG_flow_name,
-        "sg_path": SG_path,
+        "select_phrases": "edits/select_phrases.json",
+        "sg_flow_id": "b83315a6-b25c-413a-9aa0-953bf60f223c",
+        "sg_flow_name": "safeguarding_wfr_interaction",
+        "sg_sources": [
+            {"key": "zul", "path": "excel_files/safeguarding zulu.xlsx"},
+            {"key": "hau", "path": "excel_files/safeguarding swati.xlsx"}
+        ],
         "sources": sources,
-        "special_expiration": special_expiration,
-        "special_words": special_words,
-        "translation_repo": translation_repo,
-        "transl_edits_sheet_id": transl_edits_sheet_ID,
+        "special_expiration": "edits/specific_expiration.json",
+        "special_words": "edits/special_words.json",
+        "translation_repo": "https://github.com/IDEMSInternational/plh-digital-content",
+        "transl_edits_sheet_id": "1fCLPfiqHy1nLLqh1qyvd3zrziw5Tz3uQ6_e7CyuEW-E",
     }
